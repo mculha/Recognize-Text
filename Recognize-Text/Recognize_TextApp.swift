4e4dc2c0735fb98c06ad676bd9059fae34ac6320
mculha <melih.culha.09@gmail.com>
2024-01-11 22:19:32 +0300

    Initial Commit

---
+++ Recognize-Text/Recognize_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recognize_TextApp.swift
+//  Recognize-Text
+//
+//  Created by Melih Çulha on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Recognize_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
